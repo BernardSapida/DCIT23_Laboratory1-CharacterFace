52100950aad8de3b0d13c9ae519752fd6813ac22
BernardSapida <bernardsapida1706@gmail.com>
2022-06-08 13:14:54 +0800

    commit

---
+++ src/App.java
@@ -0,0 +1,42 @@
+/* Author: Bernard Sapida */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\u001B[36m");
+        System.out.println("1011010  1011010  101101   101   00  1011010 101101  101101  ");
+        System.out.println("00   00  00       00   00  1010  00  00   00 00   00 00   00 ");
+        System.out.println("101101   10100    101101   00 00 00  1011010 101101  00   00 ");
+        System.out.println("00   00  00       00   00  00  1010  00   00 00   00 00   00 ");
+        System.out.println("1011010  1011010  00   00  00   101  00   00 00   00 101101  ");
+        System.out.println("\u001B[31m");
+        System.out.println("      1011010  1011010  101101   00  101101    10100");
+        System.out.println("      00       00   00  00   00  00  00   00  00   00 ");
+        System.out.println("      1011010  1011010  101101   00  00   00  1011010 ");
+        System.out.println("           00  00   00  00       00  00   00  00   00 ");
+        System.out.println("      1011010  00   00  00       00  101101   00   00 ");
+        System.out.println("\u001B[34m");
+        System.out.println("                         101     101");
+        System.out.println("                       1011010 1011010");
+        System.out.println("                       00   10100   00");
+        System.out.println("                      00 00  101 00  00");
+        System.out.println("                      00 00  101 00  00");
+        System.out.println("                      00     101     00");
+        System.out.println("                      101   10100   101");
+        System.out.println("                     1011011011011011010");
+        System.out.println("                    1011010  00  10110100");
+        System.out.println("                   10110110110110110110100");
+        System.out.println("                  101101             101101");
+        System.out.println("                  00                     00");
+        System.out.println("                  0      101101101101     0");
+        System.out.println("                 00       1011011010      00");
+        System.out.println("                 00        10110100        00");
+        System.out.println("                101         101101         101");
+        System.out.println("               10100         1010        10100");
+        System.out.println("               10100          00         10100");
+        System.out.println("                10100                   10100");
+        System.out.println("                 101101               1011010");
+        System.out.println("                101101101101101101101101101101");
+        System.out.println("               101 101                 101 1010");
+        System.out.println("                   101                      101");
+    }
+}
